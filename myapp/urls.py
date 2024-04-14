a9761d5932283eacf40dd79f1a60463ad5afb10c
OpSlimepc <dluisvictor1@gmail.com>
2024-04-14 21:18:04 +0800

    integrated the model

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
     path("teammembers/", views.team_members, name="teammembers"),
-     path("chat/", views.chat, name="chat"),
+    path("chat/", views.chat, name="chat"),
+    path("getResponse", views.getResponse, name='getResponse'),
 ]
