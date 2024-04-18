468ebb39cd5e47b97691b979795c5dbaa1f0463f
OpSlimepc <dluisvictor1@gmail.com>
2024-04-18 22:35:21 +0800

    updated

--- motibotdjango/settings.py
+++ motibotdjango/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY","secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG",True)
 
-ALLOWED_HOSTS = [".onrener.com"]
+ALLOWED_HOSTS = [".onrener.com", "localhost"]
 
 
 # Application definition
