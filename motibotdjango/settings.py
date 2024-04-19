3d97edf3939f9e7339dbfc01d1bc79d4d9baccde
Akintuntun <119497959+Akintuntun@users.noreply.github.com>
2024-04-19 12:25:07 +0800

    FInal(for deployment)

--- motibotdjango/settings.py
+++ motibotdjango/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY","secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG",True)
 
-ALLOWED_HOSTS = [".onrender.com", "localhost", "motibot-django-final-1.onrender.com"]
+ALLOWED_HOSTS = [".onrender.com", "localhost", "motibot-django-final-1.onrender.com","127.0.0.1"]
 
 
 # Application definition
