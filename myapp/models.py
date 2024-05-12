12775340efe9c80896223eb930c10b10b39bfb0b
OpSlimepc <dluisvictor1@gmail.com>
2024-05-12 21:26:14 +0800

    admin-database-related

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Directory(models.Model):
+    address = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    contact_info = models.CharField(max_length=255)
+    services = models.TextField()
+
+    def __str__(self):
+        return self.name
